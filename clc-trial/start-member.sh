33bc614301b36283fdd154b7ed1fd418d0a95b0f
utkucaglayan <utku.caglayan@hazelcast.com>
2022-02-10 16:59:46 +0300

    try docker-swarm

--- clc-trial/start-member.sh
+++ clc-trial/start-member.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "Installing Hazelcast"
-wget -qO - https://repository.hazelcast.com/api/gpg/key/public | sudo apt-key add -
-echo "deb https://repository.hazelcast.com/debian stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt update && sudo apt install hazelcast -y
+#echo "Installing Hazelcast"
+#wget -qO - https://repository.hazelcast.com/api/gpg/key/public | sudo apt-key add -
+#echo "deb https://repository.hazelcast.com/debian stable main" | sudo tee -a /etc/apt/sources.list
+#sudo apt update && sudo apt install hazelcast -y
+
+
+#!/bin/bash
+echo "Starting Hazelcast"
+docker network create hazelcast-network
+docker pull hazelcast/hazelcast
+docker run \
+  -it \
+  --network hazelcast-network \
+  --rm \
+  -p 5701:5701 hazelcast/hazelcast
