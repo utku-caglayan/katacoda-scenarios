fe6c95bc30002ebc7b90b94078fcf059563d42f0
utkucaglayan <utku.caglayan@hazelcast.com>
2022-02-24 23:53:26 +0300

    upgrade go version to compile termdbms deps

--- sql-browser/install-hzcs.sh
+++ sql-browser/install-hzcs.sh
@@ -5,10 +5,10 @@ docker run -d --network hazelcast-network -p 5701:5701 hazelcast/hazelcast
 echo "Installing HZC"
 mkdir .local
 cd local
-git clone https://github.com/utku-caglayan/hzcSQLBrowser.git
-cd hzcSQLBrowser
 GO111MODULE=on go get golang.org/dl/go1.16.4@latest
 go1.16.4 download
+git clone https://github.com/utku-caglayan/hzcSQLBrowser.git
+cd hzcSQLBrowser
 git checkout termdbms-components
 go mod tidy # do not know why this is necessary
 go1.16.4 build -o browser
